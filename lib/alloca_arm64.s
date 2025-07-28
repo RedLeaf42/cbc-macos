5acdd4c2009e056c58b8cc9829aaf98b76b4c52c
刘武强 <liuwuqiang@lippu.ltd>
2025-07-28 17:37:32 +0800

    [feature] alloca函数实现

--- lib/alloca_arm64.s
+++ lib/alloca_arm64.s
@@ -1,10 +1,15 @@
-        .text
-        .globl  _alloca
-        .p2align        2
+	.text
+	.globl	_alloca
+	.p2align	2
 _alloca:
-        mov     x1, x0
-        mov     x0, sp
-        add     x1, x1, #15
-        and     x1, x1, #0xfffffffffffffff0
-        sub     sp, sp, x1
-        ret 
+	// ARM64版本的alloca实现
+	// 输入：x0 = 需要分配的字节数
+	// 输出：x0 = 分配的地址
+	// 保存链接寄存器
+	add x0, x0, #15
+	and x0, x0, #0xfffffffffffffff0
+	sub	sp, sp, x0
+	// 返回分配的地址
+	add x0, sp,16
+	// 恢复链接寄存器
+	ret
